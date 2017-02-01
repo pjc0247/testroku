03d4f56be9f84d386734730917ba300aaf15d65c
Park June Chul <pjc0247@naver.com>
2017-02-01 18:04:09 +0900

    e

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Slacker2;
+using Slacker2.Models;
+
 namespace TestApp
 {
-    class Program
+    class Program : BotService
     {
+        [Subscribe("^hi")]
+        public void OnMessage(SlackMessage message)
+        {
+            message.Reply("Hi there");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("HELLO WORLD");
-        
 
+            SlackBot.Configuration = new SlackBotConfiguration()
+            {
+                AuthToken = "xoxp-41105373671-41469381379-42639991936-7ad9c24d95"
+            };
+
+            SlackBot.Run();
+
+            Console.Read();
         }
     }
 }
